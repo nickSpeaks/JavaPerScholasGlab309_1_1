0043c111904ce0806e103117c9fc27a082ed3634
Nick Andreyeu <54830989+nickSpeaks@users.noreply.github.com>
2024-08-06 06:59:43 -0700

    result

---
+++ src/main/java/com/example/glab/GlabApplication.java
@@ -0,0 +1,14 @@
+package com.example.glab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlabApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/glab/GlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
